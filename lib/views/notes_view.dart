fb41f9373be34ea97e495ee39c9eda8b1860e707
NOUREDDINE010 <nordin.tata0@gmail.com>
2022-12-22 18:25:04 +0100

    step-2

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:news_app/views/widgets/custom_app_bar.dart';
+import 'package:news_app/views/widgets/notes_view_widget.dart';
 
 class NotesView extends StatelessWidget {
   const NotesView ({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+    body: NotesViewWidget(),
+
+    );
   }
 }
+
+
+
---
+++ lib/views/widgets/notes_view_widget.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:news_app/views/widgets/custom_app_bar.dart';
+
+class NotesViewWidget extends StatelessWidget {
+  const NotesViewWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Column(
+        children: const [
+          SizedBox(
+            height: 50,
+          ),
+          CustomAppBar(),
+        ],
+      ),
+    );
+  }
+}
